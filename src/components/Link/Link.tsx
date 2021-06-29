94d478da7be9a30f2225d6d134f43be6ebd69a1e
Feras Aloudah <fireslay@gmail.com>
2021-06-29 20:09:28 +0300

    feat: added a composed Link component

---
+++ src/components/Link/Link.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { PropsWithChildren } from 'react';
+
+import { Link as ChakraLink, LinkProps as ChakraLinkProps } from '@chakra-ui/react';
+import { LinkProps as NextLinkProps } from 'next/dist/client/link';
+import NextLink from 'next/link';
+
+export type LinkProps = PropsWithChildren<Omit<ChakraLinkProps & NextLinkProps, 'as'>>;
+
+export const Link: React.FC<LinkProps> = (props) => {
+  return <ChakraLink as={NextLink} {...props} />;
+};
+
+export default Link;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export * from './Motion';
+export * from './Link';
