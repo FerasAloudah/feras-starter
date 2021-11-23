e020e6bb66a42e371b74a36489296cbbcdb968d9
FerasAloudah <fireslay@gmail.com>
2021-11-24 00:12:55 +0300

    feat: add actions addon to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-essentials', '@storybook/addon-links'],
   core: {
     builder: 'webpack5',
   },
