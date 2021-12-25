05a0bfbd3e95a6e6239a74c5626a3c3d0e4e040f
FerasAloudah <fireslay@gmail.com>
2021-12-26 00:00:11 +0300

    feat: add more constants to utils

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,5 @@
 export const __DEV__ = process.env.NODE_ENV === 'development';
+export const __TEST__ = process.env.NODE_ENV === 'test';
+export const __PROD__ = process.env.NODE_ENV === 'production';
+export const __SERVER__ = typeof window === 'undefined';
+export const __BROWSER__ = typeof window !== 'undefined';
