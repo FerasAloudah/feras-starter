504ee35ded5cba9e644aa60961d2d811db76411f
FerasAloudah <fireslay@gmail.com>
2021-12-25 23:12:57 +0300

    feat: replace useTranslation with useRouter in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,27 @@
 import * as React from 'react';
 
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-import { appWithTranslation, useTranslation } from 'next-i18next';
+import { appWithTranslation } from 'next-i18next';
 import { DefaultSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import { getLangDir } from 'rtl-detect';
 
 import { siteConfig } from 'configs/site-config';
 import theme from 'theme';
 
 const MyApp = (props: AppProps) => {
   const { Component, pageProps } = props;
-  const { i18n } = useTranslation();
+  const router = useRouter();
+  const locale = router.locale as string;
+  const direction = getLangDir(locale);
 
   React.useEffect(() => {
-    document.documentElement.setAttribute('dir', i18n.dir());
-    document.cookie = `NEXT_LOCALE=${i18n.language}`;
-  }, [i18n]);
+    document.documentElement.setAttribute('dir', direction);
+    document.cookie = `NEXT_LOCALE=${locale}`;
+  }, [direction, locale]);
 
   return (
     <>
@@ -25,8 +30,8 @@ const MyApp = (props: AppProps) => {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link href="/favicon.ico" rel="icon" />
       </Head>
-      <DefaultSeo {...siteConfig.seo} openGraph={{ ...siteConfig.seo, locale: i18n.language }} />
-      <ChakraProvider theme={extendTheme({ direction: i18n.dir() }, theme)}>
+      <DefaultSeo {...siteConfig.seo} openGraph={{ ...siteConfig.seo, locale }} />
+      <ChakraProvider theme={extendTheme({ direction }, theme)}>
         <Component {...pageProps} />
       </ChakraProvider>
     </>
