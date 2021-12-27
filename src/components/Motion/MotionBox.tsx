7bdb365b0a74b71fceea29f1dd8e13a6328edf67
FerasAloudah <fireslay@gmail.com>
2021-12-27 03:39:40 +0300

    fix: use correct MotionBox imports

--- src/components/Motion/MotionBox.tsx
+++ src/components/Motion/MotionBox.tsx
@@ -6,6 +6,8 @@ import { __DEV__ } from 'utils/constants';
 
 export const MotionBox: CustomDomComponent<PropsOf<typeof chakra.div>> = motion(chakra.div);
 
+export type MotionBoxProps = PropsOf<typeof MotionBox>;
+
 if (__DEV__) {
   MotionBox.displayName = 'MotionBox';
 }
