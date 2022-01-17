34ab80d6a4e90d04056db2f9c2bf49a6c80237c8
FerasAloudah <fireslay@gmail.com>
2022-01-17 08:52:35 +0300

    feat: configure msw globally with jest

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,7 +2,17 @@ import '@testing-library/jest-dom/extend-expect';
 import 'jest-extended';
 import { toMatchOneOf, toMatchShapeOf } from 'jest-to-match-shape-of';
 
+import { server } from './src/mocks/server';
+
 expect.extend({
   toMatchOneOf,
   toMatchShapeOf,
+});
+
+beforeEach(() => {
+  server.listen({ onUnhandledRequest: 'bypass' });
+});
+
+afterEach(() => {
+  server.close();
 });
